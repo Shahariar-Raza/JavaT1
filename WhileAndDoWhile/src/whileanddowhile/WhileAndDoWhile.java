0124dfd199dd6aece8d5fd45ef6793098e7a1300
Shahariar-Raza <shahariarraza@gmail.com>
2019-10-14 11:47:34 +0600

    While and do-While loop

---
+++ WhileAndDoWhile/src/whileanddowhile/WhileAndDoWhile.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package whileanddowhile;
+
+/**
+ *
+ * @author Shaha
+ */
+public class WhileAndDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int a = 0;
+         while(a<=10){
+         System.out.println(a);
+         a++;
+         }
+         System.out.println("......................................................");
+        
+         int b = 0;
+         do{
+             System.out.println(b);
+             b++;
+         }while(b<=10);
+        System.out.println("......................................................"); 
+                  int x = 0;
+         while(x<= -1){
+         System.out.println(x);
+         x++;
+         }
+         System.out.println("......................................................");
+        
+         int y = 0;
+         do{
+             System.out.println(y);
+             y++;
+         }while(b<= -1);
+    }
+    
+}
