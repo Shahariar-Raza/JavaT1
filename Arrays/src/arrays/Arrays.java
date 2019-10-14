f8b94c44d9b5303fe3b2accb733111eb7e35683f
Shahariar-Raza <shahariarraza@gmail.com>
2019-10-14 12:08:15 +0600

    Arrays

---
+++ Arrays/src/arrays/Arrays.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrays;
+
+/**
+ *
+ * @author Shaha
+ */
+public class Arrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int [] myarray = {13,24,31,42,50};
+         
+         int index = 0;
+         while(index<5){
+         System.out.println(myarray[index]);
+         index ++;
+    
+         }
+    }
+}
