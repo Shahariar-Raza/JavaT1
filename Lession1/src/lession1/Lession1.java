a6018fda90b21424bb569377b2cd03e359988a5f
Shahariar-Raza <shahariarraza@gmail.com>
2019-10-14 10:09:54 +0600

    First Lession

---
+++ Lession1/src/lession1/Lession1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lession1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Shaha
+ */
+public class Lession1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Some Number :");
+        double user_input = scan.nextDouble();
+        
+        System.out.println("The Number is :" + user_input);
+    }
+    
+}
